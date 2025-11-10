3ad01f9e02c764386b7eeaaa42e8634c505d131a
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2025-11-09 22:14:03 -0300

    Complete Products CRUD
    10.7 Deleting Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,7 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  # CRUD Products
-  # resources :products
-
-  get "/products", to: "products#index"
-
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
-
-  get "/products/:id", to: "products#show"
-
-  get "/products/:id/edit", to: "products#edit"
-  put "/products/:id", to: "products#update"
-  patch "/products/:id", to: "products#update"
-
-  delete "/products/:id", to: "products#destroy"
-
+  resources :products
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
