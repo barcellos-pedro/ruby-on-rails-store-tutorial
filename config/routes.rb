58bb7268ff838aa376515b774d5c2e8391fb9a4e
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2025-11-10 17:48:09 -0300

    Add Subscribers to Products
    16.2. Adding Subscribers to Products

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   root "products#index"
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
