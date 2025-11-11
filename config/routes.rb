ac0c0d2f76c0dcc7e3430d3ed9a21ebf32417779
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2025-11-10 21:01:10 -0300

    Implement Product Notification Unsubscribe
    16.5. Unsubscribe Links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  root "products#index"
+
   resource :session
   resources :passwords, param: :token
-  root "products#index"
 
   resources :products do
     resources :subscribers, only: [ :create ]
   end
+
+  resource :unsubscribe, only: [ :show ]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
